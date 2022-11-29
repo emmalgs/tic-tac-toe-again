97a2dcc379859764a8722f8124387203f9bb409c
Emma Gerigscott <elgerig@gmail.com>
2022-11-29 14:30:55 -0800

    start game function

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ const winCombos = [
 ]
 
 const cells = document.querySelectorAll('.cell')
+startGame()
+
+function startGame() {
+    document.querySelector('.endgame').style.display = 'none'
+    origBoard = Array.from(Array(9).keys())
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false)
+    }
+}
